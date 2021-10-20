ec9bdb4938ec211fd6296877b2c040e95d4ffb46
Chetan Padia <chetbox@users.noreply.github.com>
2021-10-20 09:41:38 +0100

    repro for sentry `Object(...) is not a function`

---
+++ webpack.rules.js
@@ -0,0 +1,27 @@
+module.exports = [
+  // Add support for native node modules
+  {
+    // We're specifying native_modules in the test because the asset relocator loader generates a
+    // "fake" .node file which is really a cjs file.
+    test: /native_modules\/.+\.node$/,
+    use: 'node-loader',
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      options: {
+        outputAssetBase: 'native_modules',
+      },
+    },
+  },
+  {
+    test: /\.(js|ts)$/,
+    loader: 'babel-loader',
+    options: {
+      minified: true,
+      presets: ['@babel/preset-typescript']
+    }
+  },
+];
